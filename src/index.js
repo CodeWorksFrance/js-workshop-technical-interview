421dea1fcdd4d0675da7c2d026e5b01837a44f5e
Christophe Thibaut <cthibaut@octo.com>
2022-06-19 21:10:42 +0200

    feature: simple dialog on console with readline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("hello, world!")
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('What is your name?', (name) => {
+    rl.question('Where do you live?', (country) => {
+        console.log(`${name}, is a citizen of ${country}`);
+    })
+});
+
+rl.on('close', () => {
+    console.log('\nBYE BYE!');
+    process.exit(0);
+})
+
