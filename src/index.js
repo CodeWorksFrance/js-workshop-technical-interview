6bf700afc8a40a895ef41168ee8ae9c31aa0b232
Christophe Thibaut <cthibaut@octo.com>
2022-06-19 21:54:05 +0200

    feature: prompt, with same redirect input using a test file (just type: npm start <test/data/sample_input.txt)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const prompt = require('prompt');
+
+const properties = [
+    {
+        name: 'username',
+        validator: /^[a-zA-Z\s-]+$/,
+        warning: 'Username must be only letters, spaces, or dashes.'
+    },
+    {
+        name: 'password',
+        hidden: true
+    }
+];
+
+prompt.start();
+
+prompt.get(['username', 'password'], (err,result) => {
+    if(err) {
+        return onErr(err);
+    }
+    console.log('Command-line input received:');
+    console.log('  Username: ' + result.username);
+    console.log('  Password: ' + result.password);
 });
 
-rl.question('What is your name?', (name) => {
-    rl.question('Where do you live?', (country) => {
-        console.log(`${name}, is a citizen of ${country}`);
-    })
-});
-
-rl.on('close', () => {
-    console.log('\nBYE BYE!');
-    process.exit(0);
-})
-
+const onErr = (err) => {
+    console.log(err);
+    return 1;
+}
